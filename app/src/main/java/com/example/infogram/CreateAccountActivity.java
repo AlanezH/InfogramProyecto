96b0b102c0e0f46abda627e7c72878d362f665f2
AlanezH <alaneshilder123@gmail.com>
2025-09-14 10:24:52 -0400

    toolbar

--- app/src/main/java/com/example/infogram/CreateAccountActivity.java
+++ app/src/main/java/com/example/infogram/CreateAccountActivity.java
@@ -1,9 +1,11 @@
 package com.example.infogram;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +17,19 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
+
+        showToolBar(getString(R.string.crear_cuenta), true);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void showToolBar(String Titulo , boolean botonSubir){
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(Titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
     }
 }
